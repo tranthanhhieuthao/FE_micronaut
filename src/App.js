dd711d2c3142d3ff3d422e2ac9890db596395fee
tranhieu <tranthanhhieuthao@gmail.com>
2021-06-08 16:50:03 +0700

    sua lai layout login register

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import React, { Component } from 'react'
-import ClassWelcome from './Components/ClassWelcome'
-import { BrowserRouter,Route, Switch, Redirect } from 'react-router-dom';
-import Welcome from './Components/Welcome'
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Home from './Components/Home'
 import ListUser from './Components/ListUser'
 import DetailUser from './Components/DetailUser'
 import AdminScreen from './Components/AdminScreen'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+import { makeStyles } from '@material-ui/core/styles'
+import TopBar from './Components/TopBar'
 
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary
+  }
+}))
 function App() {
   return (
     <div>
-      <Switch>
-        <Route path="/login" component={ClassWelcome} />
-        <Route path="/register" component={Welcome} />
-        <Route path="/home" component={Home} />
-        <Route path="/users" component={ListUser} />
-        <Route path="/detail" component={DetailUser} />
-        <Route path="/admin" component={AdminScreen} />
-        <Redirect to="/" />
-      </Switch>
+      <div className={useStyles.root}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Paper className={useStyles.paper}><TopBar /></Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper style={{ height: 'calc(100vh - 200px)', marginTop: '20px' }} className={useStyles.paper}>SideBar</Paper>
+          </Grid>
+          <Grid item xs={9} >
+            <Paper style={{ height: 'calc(100vh - 200px)' }} className={useStyles.paper}>
+            
+          <Switch>
+            <Route path="/home" component={Home} />
+            <Route path="/users" component={ListUser} />
+            <Route path="/detail" component={DetailUser} />
+            <Route path="/admin" component={AdminScreen} />
+            <Redirect to="/" />
+          </Switch>
+          </Paper>
+              </Grid>
+            </Grid>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,43 +3,19 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { BrowserRouter } from 'react-router-dom'
-import TopBar from './Components/TopBar'
-import { makeStyles } from '@material-ui/core/styles'
-import Paper from '@material-ui/core/Paper'
-import Grid from '@material-ui/core/Grid'
-import { CookiesProvider } from 'react-cookie'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import ClassWelcome from './Components/ClassWelcome'
+import Welcome from './Components/Welcome'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary
-  }
-}))
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <div className={useStyles.root}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Paper className={useStyles.paper}><TopBar /></Paper>
-          </Grid>
-          <Grid item xs={3}>
-            <Paper style={{ height: 'calc(100vh - 200px)', marginTop: '20px' }} className={useStyles.paper}>SideBar</Paper>
-          </Grid>
-          <Grid item xs={9} >
-            <Paper style={{ height: 'calc(100vh - 200px)' }} className={useStyles.paper}>
-              <CookiesProvider>
+            <Switch>
+                <Route path="/login" exact component={ClassWelcome}/>
+                <Route path="/register" component={Welcome} />
                 <App />
-              </CookiesProvider>
-            </Paper>
-          </Grid>
-        </Grid>
-      </div>
+              </Switch>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
